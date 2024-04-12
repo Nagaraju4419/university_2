872c30b563dfeffba72e0a3d69996ed1e32ff175
nagaraju <rnagaraju4419@gmail.com>
2024-04-12 12:35:20 +0000

    solution

---
+++ src/main/java/com/example/university/service/StudentJpaService.java
@@ -0,0 +1,89 @@
+package com.example.university.service;
+
+import com.example.university.model.*;
+import com.example.university.repository.StudentRepository;
+import com.example.university.repository.ProfessorJpaRepository;
+import com.example.university.repository.StudentJpaRepository;
+import com.example.university.repository.CourseJpaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentJpaService implements StudentRepository {
+
+    @Autowired
+    private StudentJpaRepository studentJpaRepository;
+
+    @Autowired
+    private CourseJpaRepository courseJpaRepository;
+
+    @Override
+    public ArrayList<Student> getStudents() {
+        List<Student> studentList = studentJpaRepository.findAll();
+        ArrayList<Student> students = new ArrayList<>(studentList);
+        return students;
+    }
+
+    @Override
+    public Student getStudentById(int studentId) {
+        try {
+            Student student = studentJpaRepository.findById(studentId).get();
+            return student;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Null values adding
+    @Override
+    public Student addStudent(Student student) {
+        studentJpaRepository.save(student);
+        return student;
+    }
+
+    @Override
+    public Student updateStudent(int studentId, Student student) {
+        try {
+            Student newStudent = studentJpaRepository.findById(studentId).get();
+            if (student.getStudentName() != null) {
+                newStudent.setStudentName(student.getStudentName());
+            }
+            if (student.getEmail() != null) {
+                newStudent.setEmail(student.getEmail());
+            }
+            if (student.getCourses().size() > 0) {
+                newStudent.setCourses(student.getCourses());
+            }
+            return studentJpaRepository.save(newStudent);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    
+    @Override
+    public void deleteStudent(int studentId) {
+        try {
+            studentJpaRepository.deleteById(studentId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+    }
+
+    @Override
+    public List<Course> getStudentCourses(int studentId) {
+        try {
+            Student student = studentJpaRepository.findById(studentId).get();
+            return student.getCourses();
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+}
